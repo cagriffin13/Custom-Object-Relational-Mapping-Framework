51b6a29f63d57f1d6ceacc1583d5faa1454b2f22
cagriffin13 <96761020+cagriffin13@users.noreply.github.com>
2022-02-24 08:51:03 -0800

    Add files via upload

---
+++ project_01_ORM/src/main/java/repository/Row.java
@@ -0,0 +1,54 @@
+package repository;
+
+import models.Entity;
+
+import java.util.LinkedList;
+
+public class Row {
+    private int numCols; //number of columns in the row
+    private LinkedList<String> colNames;//names of each column
+    private LinkedList<String> values;//the value of each column
+
+    public Row(Entity table)
+    {
+        colNames = table.getColumnNames(); //All rows of the table have the same column names, but it is nicer to have this stored in rows for some functions
+        numCols = colNames.size();//number of columns is equal to the number of column names
+        values = new LinkedList<String>();//initialized as an empty LinkedList to avoid null pointer exceptions
+    }
+
+    public int getNumCols() {
+        return numCols;
+    }
+
+    public void setNumCols(int numCols) {
+        this.numCols = numCols;
+    }
+
+    public LinkedList<String> getColNames() {
+        return colNames;
+    }
+
+    public void setColNames(LinkedList<String> colNames) {
+        this.colNames = colNames;
+    }
+
+    public LinkedList<String> getValues() {
+        return values;
+    }
+
+    public void setValues(LinkedList<String> values) {
+        this.values = values;
+    }
+
+    public String toString()
+    {
+        String output ="";
+        for (int i =0;i<numCols;i++)
+        {
+            output+= values.get(i)+"\t";
+        }
+        return output;
+    }
+}
+
+
---
+++ project_01_ORM/src/main/java/util/JDBCConnection.java
@@ -0,0 +1,55 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
+
+public class JDBCConnection {
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+         /*
+        To establish a new connection if one doesn't exist,
+        otherwise return the current connection.
+
+        Credentials: url (endpoint), username, password
+         */
+
+        if (conn == null) {
+            Properties props = new Properties();
+
+            try {
+                Class.forName("org.postgresql.Driver");
+                 props.load(JDBCConnection.class.getClassLoader().getResourceAsStream("connection.properties"));
+
+
+                String endpoint = props.getProperty("endpoint");
+                String url = "jdbc:postgresql://" + endpoint + "/postgres";
+                String username = props.getProperty("username");
+                String password = props.getProperty("password");
+
+
+                conn = DriverManager.getConnection(url, username, password);//getConnection(url, username, password);
+
+            } catch (SQLException | IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        Connection conn1 = getConnection();
+        Connection conn2 = getConnection();
+
+
+        System.out.println(conn1);
+        System.out.println(conn2);
+    }
+}
